0319ef285a3f5b8e1fd38b4f0545701f19af5bb3
junruPan <junrupan@sf-express.com>
2019-03-20 17:34:45 +0800

    添加注释

--- litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/annotation/RequiresPermissionsDesc.java
+++ litemall-admin-api/src/main/java/org/linlinjava/litemall/admin/annotation/RequiresPermissionsDesc.java
@@ -5,9 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 菜单处理方案
+ */
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RequiresPermissionsDesc {
+
     String[] menu();
 
     String button();
